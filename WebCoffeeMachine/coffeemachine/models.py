0afd3557c4bf7bab2acf927d6931c97e7b69695c
EsamSharaf <asoomsharaf@hotmail.com>
2024-05-16 23:55:21 +0200

    feat: Item model added to models.py.

--- WebCoffeeMachine/coffeemachine/models.py
+++ WebCoffeeMachine/coffeemachine/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField(default=0.0)
+    description = models.TextField()
+    image = models.URLField()
+
+    def __str__(self):
+        return self.name
