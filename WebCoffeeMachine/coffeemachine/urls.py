764f1a590a6bc9b7ecb618ea5840ddee904abc58
EsamSharaf <asoomsharaf@hotmail.com>
2024-05-18 16:10:56 +0200

    feat: detail view added in views.py and urls.py.

--- WebCoffeeMachine/coffeemachine/urls.py
+++ WebCoffeeMachine/coffeemachine/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "coffeemachine"
 urlpatterns = [
     path("", views.home, name="home"),
+    # ex: /coffeemachine/Black Coffee/
+    path("<str:name>/", views.detail, name="detail"),
 ]
--- WebCoffeeMachine/coffeemachine/views.py
+++ WebCoffeeMachine/coffeemachine/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "coffeemachine/home.html")
+
+
+def detail(request, name):
+    return HttpResponse("You're looking at item %s detail page." % name)
