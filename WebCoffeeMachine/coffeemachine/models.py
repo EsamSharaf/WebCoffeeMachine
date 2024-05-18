62f80deb144b55771f65b6f77dfa25a3f0ab480e
EsamSharaf <asoomsharaf@hotmail.com>
2024-05-18 16:04:30 +0200

    refactor: models.py - image field changed to CharField.

--- WebCoffeeMachine/coffeemachine/models.py
+++ WebCoffeeMachine/coffeemachine/models.py
@@ -5,7 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField(default=0.0)
     description = models.TextField()
-    image = models.URLField()
+    image = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
