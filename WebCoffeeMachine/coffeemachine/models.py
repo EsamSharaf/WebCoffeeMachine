e63fd4b89b4fc24aa41fbd209afd53981f859123
EsamSharaf <asoomsharaf@hotmail.com>
2024-06-04 10:38:01 +0200

    feat: slug field added to Item model to capture the string in the url.

--- WebCoffeeMachine/coffeemachine/models.py
+++ WebCoffeeMachine/coffeemachine/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     price = models.FloatField(default=0.0)
     description = models.TextField()
     image = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, default='')
 
     def __str__(self):
         return self.name
